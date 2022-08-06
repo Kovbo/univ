24ae9dba2dbae9f04ceb1759369b322c188a1d18
mac <kovsbo@gmail.com>
2022-08-06 22:18:45 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("https://registry.edbo.gov.ua/api/universities/?ut=1&lc=80&exp=json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
